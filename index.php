e03fceab58917913d479b861249793e9f82740f6
adamdon <adamdon89@gmail.com>
2019-01-31 20:18:15 +0000

    jQuery testing

--- index.php
+++ index.php
@@ -2,8 +2,12 @@
      <head>
          <title>PHP Test</title>
          <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+
+
      </head>
      <body>
+        <script src="jquery_scripts.js"></script>
         <?php
             include 'logic.php';
 
@@ -12,5 +16,7 @@
 
             echo '<p>Test</p>' . $txt;
         ?>
+        <button>Get External var from PHP</button>
+        <h3>Text to be updated</h3>
      </body>
 </html>
