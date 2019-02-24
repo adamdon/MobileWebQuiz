153c0b92ddb9c3d2742fd5d62a0f1c3736f5c756
adamdon <adamdon89@gmail.com>
2019-02-24 20:09:05 +0000

    DataAccessObject refactored

--- PHP/DataAccessObject.php
+++ PHP/DataAccessObject.php
@@ -3,21 +3,6 @@
 
 class DataAccessObject
 {
-    public $question1;
-    public $question2;
-    public $question3;
-    public $question4;
-    public $question5;
-    public $question6;
-    public $question7;
-    public $question8;
-    public $question9;
-    public $question10;
-    public $question11;
-    public $question12;
-    public $question13;
-
-    public $arrayOfQuestions;
 
     public function __construct()
     {
@@ -26,32 +11,45 @@ class DataAccessObject
 
     public function setupQuestions()
     {
-        $this->question1 = new QuestionModel("What is the capital of France", "Paris");
-        $this->question2 = new QuestionModel("What is the capital of Germany", "Berlin");
-        $this->question3 = new QuestionModel("What is the capital of Italy", "Rome");
-        $this->question4 = new QuestionModel("What is the capital of Spain", "Madrid");
-        $this->question5 = new QuestionModel("What is the capital of Austria", "Vienna");
-        $this->question6 = new QuestionModel("What is the capital of Belgium", "Brussels");
-        $this->question7 = new QuestionModel("What is the capital of Czech Republic", "Prague");
-        $this->question8 = new QuestionModel("What is the capital of Denmark", "Copenhagen");
-        $this->question9 = new QuestionModel("What is the capital of Estonia", "Tallinn");
-        $this->question10 = new QuestionModel("What is the capital of Norway", "Oslo");
-        $this->question11 = new QuestionModel("What is the capital of Poland", "Warsaw");
-        $this->question12 = new QuestionModel("What is the capital of Sweden", "Stockholm");
-        $this->question13 = new QuestionModel("What is the capital of Portugal", "Lisbon");
 
 
-        $this->arrayOfQuestions = array($this->question1, $this->question2, $this->question3, $this->question4, $this->question5);
-        array_push($this->arrayOfQuestions,  $this->question6);
-        array_push($this->arrayOfQuestions,  $this->question7);
-        array_push($this->arrayOfQuestions,  $this->question8);
-        array_push($this->arrayOfQuestions,  $this->question9);
-        array_push($this->arrayOfQuestions,  $this->question10);
-        array_push($this->arrayOfQuestions,  $this->question11);
-        array_push($this->arrayOfQuestions,  $this->question12);
-        array_push($this->arrayOfQuestions,  $this->question13);
+        return $this->CapitalCityQuestions();
+    }
 
-        return $this->arrayOfQuestions;
+
+    private function CapitalCityQuestions()
+    {
+        $arrayOfQuestions = [];
+
+        $question1 = new QuestionModel("What is the capital of France", "Paris");
+        $question2 = new QuestionModel("What is the capital of Germany", "Berlin");
+        $question3 = new QuestionModel("What is the capital of Italy", "Rome");
+        $question4 = new QuestionModel("What is the capital of Spain", "Madrid");
+        $question5 = new QuestionModel("What is the capital of Austria", "Vienna");
+        $question6 = new QuestionModel("What is the capital of Belgium", "Brussels");
+        $question7 = new QuestionModel("What is the capital of Czech Republic", "Prague");
+        $question8 = new QuestionModel("What is the capital of Denmark", "Copenhagen");
+        $question9 = new QuestionModel("What is the capital of Estonia", "Tallinn");
+        $question10 = new QuestionModel("What is the capital of Norway", "Oslo");
+        $question11 = new QuestionModel("What is the capital of Poland", "Warsaw");
+        $question12 = new QuestionModel("What is the capital of Sweden", "Stockholm");
+        $question13 = new QuestionModel("What is the capital of Portugal", "Lisbon");
+
+        array_push($arrayOfQuestions, $question1);
+        array_push($arrayOfQuestions, $question2);
+        array_push($arrayOfQuestions, $question3);
+        array_push($arrayOfQuestions, $question4);
+        array_push($arrayOfQuestions, $question5);
+        array_push($arrayOfQuestions, $question6);
+        array_push($arrayOfQuestions, $question7);
+        array_push($arrayOfQuestions, $question8);
+        array_push($arrayOfQuestions, $question9);
+        array_push($arrayOfQuestions, $question10);
+        array_push($arrayOfQuestions, $question11);
+        array_push($arrayOfQuestions, $question12);
+        array_push($arrayOfQuestions, $question13);
+
+        return $arrayOfQuestions;
     }
 
 }
