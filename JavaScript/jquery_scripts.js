8a2af465688ab7735730edb0988d304a1fcae21b
adamdon <adamdon89@gmail.com>
2019-02-27 12:23:36 +0000

    javascript refactored

--- JavaScript/jquery_scripts.js
+++ JavaScript/jquery_scripts.js
@@ -1,12 +1,7 @@
 function startSession()
 {
     let strURL = "Interface.php?request=startSession&p1=0&p2=0";
-
-    $.get
-    (strURL, function(data)
-    {
-        $('#content').html(data);
-    });
+    requestContent(strURL);
 }
 
 function logIn()
@@ -14,50 +9,40 @@ function logIn()
     let strEmail = $('#email').val();
     let strPassword= $('#pass').val();
 
-
     let strURL = "Interface.php?request=logIn&p1=" + strEmail + "&p2=" + strPassword;
 
-    $.get
-    (strURL, function(data)
-    {
-        $('#content').html(data);
-    });
+    requestContent(strURL);
 }
 
 function startGame()
 {
     let intRoundsSelectedNum = $('#selectRounds').val();
     let strRoundsSelectedNum = intRoundsSelectedNum.toString();
-
     let strCategorySelected = $('#selectCategory').val();
 
     let strURL = "Interface.php?request=newGame&p1=" + strRoundsSelectedNum + "&p2=" + strCategorySelected;
 
-    $.get
-    (strURL, function(data)
-    {
-        $('#content').html(data);
-    });
+    requestContent(strURL);
 }
 
 function submitAnswer()
 {
     let radioSelected = $('input[name=options]:checked').val();
 
-
     let strURL = "Interface.php?request=submitAnswer&p1=" + radioSelected + "&p2=0";
 
-    $.get
-    (strURL, function(data)
-    {
-        $('#content').html(data);
-    });
+    requestContent(strURL);
 }
 
 function nextRound()
 {
     let strURL = "Interface.php?request=nextRound&p1=0&p2=0";
 
+    requestContent(strURL);
+}
+
+function requestContent(strURL)
+{
     $.get
     (strURL, function(data)
     {
