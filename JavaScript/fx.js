7ab71c88eee2ca6b8290c8037e34ce45f36b67c5
Andreea <andreeacora98@gmail.com>
2019-03-05 12:02:13 +0000

    fixed auto fx height; added register page using the login page as template

--- JavaScript/fx.js
+++ JavaScript/fx.js
@@ -1,7 +1,8 @@
 $(document).ready(function(){
   let $card = $('.card form');
-  let cardHeight = $card.height();
+  let cardHeight = $card[0].offsetHeight;
   $card.height(0);
+  
   $('.card form').delay(400).animate({
     height: cardHeight
   }, {
